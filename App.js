8c8499a8d4cc22ca96f3be0b464b9e7c17e7c680
julien <jule64@gmail.com>
2022-12-12 00:04:50 +0000

    Migrate to Binance api
    Replace all FTX api calls to Binance API
    We use tiagosiebler Binance connectors to handle
    all rest and websocket calls.
    Operations that we migrated are:
    1. place market + limit order + handle WS execution notification (see `formattedUserDataMessage` channel)
    2. handle trade execution events received on the 'aggTrade' websocket channel
    3. query account balances per coin and update balances in UI
    
    We also introduce a stats/trade count feature that counts all the trades
    per ticker on the watchlist that executed over a 30second timeframe and
    displays the data on the front end (see 'TC_30s' column)

--- App.js
+++ App.js
@@ -7,33 +7,36 @@ const http = require('http').createServer(app);
 const socketio = require('socket.io')(http);
 app.use(express.static(__dirname));
 
-const {OrderManager, Logger, CoinUtils} = require("./AppUtils");
-
-const apikeys = require('./apikeys/apikeys.json');
-if(apikeys.key === "" || apikeys.secret === "") {
-    Logger.warn("Missing API keys.  Please add your api key & secret in ./apikeys/apikeys.json");
-    process.exit();
-}
+const {OrderManager, Logger, CoinUtils, OrdersUtils} = require("./AppUtils");
 
 
-const {WebsocketClient, RestClient} = require("ftx-api");
+const apikey = process.env.APIKEY || '';
+const apisecret = process.env.APISECRET || '';
+const appPort = process.env.APP_PORT || 5001
 
-const ftxWS = new WebsocketClient({key: apikeys.key, secret: apikeys.secret });
-const ftxRestCli = new RestClient(apikeys.key, apikeys.secret);
+Logger.info(`Starting app with:`);
+Logger.info(`apikey=${apikey}`);
+Logger.info(`apisecret=${apisecret}`);
+Logger.info(`appPort=${appPort}`);
 
-const PnL = require('./PnL');
+const { MainClient, WebsocketClient} = require('binance');
+
+const restClient = new MainClient({
+  api_key: apikey,
+  api_secret: apisecret,
+});
+
+const wsClient = new WebsocketClient(
+  {
+    api_key: apikey,
+    api_secret: apisecret,
+    beautify: true,
+  }
+);
+
 const appData = require("./resources/appData.json");
 
-const Optional = require('optional-js');
 const Deque = require("collections/deque");
-
-initApp(socketio, ftxWS, ftxRestCli);
-
-const appPort = 5001
-http.listen(appPort, () => {
-  Logger.info(`Open browser on http://localhost:${appPort}`);
-
-});
 
 let subscribedTickers = []
 const upalerts = new Map();
@@ -41,14 +44,24 @@ const downalerts = new Map();
 
 const bookCostmap = new Map();
 
-const tickersToLast10VolQueues = new Map();
-const VOLUMES_QUEUE_SIZE = 20
+const tickersToVolObjs = new Map();
+const MAX_VOLUMES_QUEUE_SIZE = 30
+
+const tradeCountsPerMinute = new Map(); // Map<Ticker, TradeCounts>
 
 const balanceObj = {
     totalUSDBalance: null,
     balancesMap: new Map()
-    }
+};
 
+
+initApp(socketio, wsClient, restClient);
+
+
+http.listen(appPort, () => {
+  Logger.info(`Open browser on http://localhost:${appPort}`);
+
+});
 
 
 
@@ -70,114 +83,178 @@ function getAlertsForPrice(priceUpdate) {
     return alertObj;
 }
 
-function buildAccountAndCoinBalances(allBalances, coinsToUpdate) {
+async function buildAccountAndCoinBalances(rc, allBalances, coinsToUpdate) {
 
-    Logger.info('updating account and coin balances');
+    try {
+        const coinToCoinBalanceMap = allBalances.reduce((p, c) => {
+            p.set(c.coin, {coin: c.coin, balance: c.free, usdValue: null});
+            return p;
+        }, new Map());
 
-    const coinToCoinBalanceMap =  allBalances.result.reduce((p, c) => {
-        p.set(c.coin,c);
-        return p;}, new Map());
+        for (const elem of coinToCoinBalanceMap.values()) {
+            const usdPrice = await getCoinUsdValue(rc, elem.coin);
+            elem.usdValue = elem.balance * usdPrice;
+        }
 
-    var res = {
-        totalUSDBalance: allBalances.result.reduce((p, c) => p + c.usdValue, 0),
-        balancesMap: coinsToUpdate.map(t => [t, coinToCoinBalanceMap.get(CoinUtils.parseCoinFromTicker(t))])
-    };
 
-    return res;
+        var res = {
+            totalUSDBalance: coinToCoinBalanceMap.valuesArray().reduce((p, c) => p + c.usdValue, 0),
+            balancesMap: coinsToUpdate.map(t => [t, coinToCoinBalanceMap.get(CoinUtils.parseCoinFromTicker(t))])
+        };
+
+        return res;
+
+    } catch(e){
+
+        throw e;
+
+    }
 }
 
-async function getBalances(rc) {
-    return Optional.ofNullable(await rc.getBalances()
-        .then(v => v)
-        .catch(e => {
-            Logger.warn("ERROR Getting Balances from FTX", e.message);
-            return null;
-        }));
+async function getCoinUsdValue(rc, coin) {
+
+    let usdValue;
+
+    if(coin === 'BUSD'){
+        usdValue = 1;
+    } else {
+        try {
+            const resp = await rc.getSymbolPriceTicker({symbol: coin+'BUSD'});
+            usdValue = resp.price;
+        } catch(e){
+            throw e
+        }
+    }
+
+    return usdValue;
 }
+
+
 
 function updateAppStateBalances(balancesJson) {
     balanceObj.totalUSDBalance = balancesJson.totalUSDBalance;
     balanceObj.balancesMap = balancesJson.balancesMap;
+    return balanceObj;
 }
 
 function initApp(sio, ws, rc) {
 
     Logger.info("initialising app");
 
-    // we add USD as a "ticker". This will give us the cash balance on the account
+    // we add BUSD as a "ticker". This will give us the cash balance on the account
     const coinsToUpdate = appData.tickerWatchlist.concat([appData.accountCcy]);
 
-
-    getBalances(rc).then(v =>
-        // initial account balances state
-        v.map(balances =>
-            buildAccountAndCoinBalances(balances, coinsToUpdate))
-            .map(balancesJson => updateAppStateBalances(balancesJson)));
+    Logger.info('updating account and coin balances');
+    CoinUtils.getNonNullBalances(rc).then(bal => {
+        buildAccountAndCoinBalances(rc, bal, coinsToUpdate).then(balancesJson => updateAppStateBalances(balancesJson));
+    });
 
     setInterval(() => {
-        getBalances(rc).then(v =>
-            v.map(balances => buildAccountAndCoinBalances(balances, coinsToUpdate))
-                .map(balancesJson => {
-                    updateAppStateBalances(balancesJson);
-                    Logger.info('sending account and coin balances');
-                    sio.emit('balances:update', balancesJson);
-                }));
 
-    }, 60000);
+        CoinUtils.getNonNullBalances(rc)
+            .then(bal => { buildAccountAndCoinBalances(rc, bal, coinsToUpdate)
+                .then(balancesJson => updateAppStateBalances(balancesJson))
+                .then((balancesJson) => sio.emit('balances:update', balancesJson))});
+
+        }, 60000);
 
 
-    Logger.log("subscribing to fills");
-    ws.subscribe({channel: 'fills'});
+    // initialise trade counts map
+    appData.tickerWatchlist.forEach(t => tradeCountsPerMinute.set(t, 0));
+
+
+    // publish and reset trade counts map every N seconds
+    setInterval(() => {
+        sio.emit('tradeStats:update', tradeCountsPerMinute);
+        appData.tickerWatchlist.forEach(t => tradeCountsPerMinute.set(t, 0));
+    }, 30000);
+
+
+    ws.subscribeSpotUserDataStream();
 
     const om = new OrderManager(rc);
     sio.on('connection', (socket) => {
 
         Logger.info('received socket connection');
-
-        Logger.info('sending account and coin balances');
         sio.emit('balances:update', balanceObj);
 
-        socket.on('price:subs', function(tickersRequested) {
+        socket.on('price:subs', function (tickersRequested) {
 
+            tickersNotSubscribedYet = tickersRequested.filter(v => !subscribedTickers.includes(v));
 
-          tickersNotSubscribedYet = tickersRequested.filter(v => !subscribedTickers.includes(v));
-          Logger.log('subscribing to live price updates for ', tickersNotSubscribedYet);
+            tickersNotSubscribedYet.map((ticker) => {
+                try {
+                    Logger.log("subscribing to real time price updates for ", ticker);
+                    ws.subscribeSpotAggregateTrades(CoinUtils.convertToBinanceTicker(ticker));
+                } catch (e) {
+                    Logger.warn("couldn't subscribe to ticker ", ticker);
+                }
+            });
+            subscribedTickers = subscribedTickers.concat(tickersNotSubscribedYet);
+            //PnL.buildCoinCostMap(tickersNotSubscribedYet, bookCostmap, ftxRestCli);
 
-          topicsTrades = tickersNotSubscribedYet.map((v) => {
-              return {channel: 'trades',
-                        market: v}
-          });
-
-          subscribedTickers = subscribedTickers.concat(tickersNotSubscribedYet);
-          try {
-            ws.subscribe(topicsTrades);
-            PnL.buildCoinCostMap(tickersNotSubscribedYet, bookCostmap, ftxRestCli);
-          } catch(e) {
-              Logger.warn("couldn't subscribe to ticker: ", e);
-          }
         });
 
-        socket.on('orders:new-market-order', (marketOrder) => {
-            Logger.info('received market order', marketOrder);
-            om.placeMarketOrder(marketOrder).then(
+        socket.on('orders:new-market-order', (order) => {
+            Logger.info('received market order', order);
+            const marketOrder = OrdersUtils.convertToBinanceMarketOrder(order);
+            om.placeOrder(marketOrder).then(
                 resp => Logger.info('order placed', resp)
-            ).catch(err =>  Logger.warn('order placement failed', err))
+            ).catch(err => Logger.warn('order placement failed', err))
         });
-        socket.on('orders:new-limitbook-order', (limitOrder) => {
-            Logger.info("placing limit order for " + limitOrder.market, limitOrder);
-            om.placeLimitOrderFromOrderBook(limitOrder.market, limitOrder.side, limitOrder.size);
+
+        socket.on('orders:new-limitbook-order', async (order) => {
+
+            Logger.info("received limit order", order);
+            const limOrder = OrdersUtils.convertToBinanceLimitOrder(order);
+
+            let res;
+            try {
+                const book = await rc.getOrderBook({symbol:limOrder.symbol});
+
+                const orderSide = limOrder.side
+
+                if (orderSide === 'BUY') {
+                    limitPrice = book.bids[2][0]; // get the third best price on the order book and use it as the order's limit price
+                } else if (orderSide === 'SELL') {
+                    limitPrice = book.asks[2][0];
+                } else {
+                    throw new Error("Order must be a BUY or SELL. Received a " + orderSide);
+                }
+
+                limOrder.price = limitPrice;
+
+                res = await om.placeOrder(limOrder);
+
+                Logger.info("Limit order placed", res);
+
+            } catch(e) {
+                Logger.warn("Order was not placed", e);
+            }
+
         });
 
         socket.on('orders:cancel-orders', async (tkr) => {
             Logger.info("cancelling all pending orders for " + tkr);
-            const resp = await om.getOpenOrders(tkr);
-            if(resp.result.length === 0) {
-                Logger.log("no pending orders found for " + tkr);
-            } else {
-                resp.result.map(async o => {
-                    Logger.log("cancelling order with id " + o.id);
-                    await om.cancelOpenOrder(o.id);
-                });
+
+            const ticker = CoinUtils.convertToBinanceTicker(tkr);
+
+
+            try {
+                const res = await om.getOpenOrders(ticker);
+
+                if (res.length === 0) {
+                    Logger.log("no pending orders found for " + ticker);
+                } else {
+                    res.map(o => {
+                        rc.cancelOrder({symbol: o.symbol, orderId: o.orderId})
+                            .then(resp => Logger.log("order cancelled", resp))
+                            .catch(err => Logger.log("cancellation rejected", err));
+                    });
+                }
+
+            } catch (e) {
+                Logger.warn("Orders did not cancel", e);
             }
         });
 
@@ -196,100 +273,114 @@ function initApp(sio, ws, rc) {
 
     });
 
+    ws.on('formattedUserDataMessage', (data) => {
 
-
-
-
-    ws.on('response', response => {
-        Logger.log('received a response from ftx: ', response);
+        if(data.eventType === 'executionReport' && data.orderStatus === 'FILLED'){
+            console.info('received a new order for ' + data.symbol);
+        } else if(data.eventType === 'executionReport' && data.orderStatus === 'NEW'){
+            Logger.info("received a fill for: "+ data.symbol);
+            const ticker = CoinUtils.convertFromBinanceTicker(data.symbol);
+            const fill = {
+                market: ticker,
+                size: data.quantity,
+            };
+            sio.emit('orders:fill', fill);
+        } else {
+            console.info('formattedUserDataMessage channel.  unprocessed event: ', data);
+        }
     });
 
-    ws.on('update', async data => {
-        if(data.type === "subscribed") {
-            Logger.log('received a subscription confirmation for', data);
-        } else if (data.type === "update") {
 
-            if(data.channel === "trades") {
-                const priceUpdate = handlePriceUpdate(data);
-                const alerts = getAlertsForPrice(priceUpdate);
-                sio.emit('price:update', [priceUpdate, alerts]);
+    ws.on('message', (data) => {
+        handleEvent("aggTrade", "spot", data, handleSpotAggregateTrades);
+    });
 
-            } else if(data.channel === "fills") {
 
-                Logger.info("received a fill for: ", data);
-                const ticker = data.data.market;
-                const fill = {
-                    market: ticker,
-                    size: data.data.size
-                };
-                sio.emit('orders:fill', fill);
-
-                const bal = await getBalances(rc);
-
-                bal.map(balances => buildAccountAndCoinBalances(balances, [ticker]))
-                    .map(balancesJson => {
-                        Logger.info('sending account and coin balances');
-                        sio.emit('balances:update', balancesJson);
-                    });
-
-                bal.map(async balances => {
-                    Logger.info('update book cost for coin');
-                    ftxRestCli.getOrderHistory({market: ticker})
-                        .then(orderHistory =>
-                            PnL.buildCoinBookCost(CoinUtils.parseCoinFromTicker(ticker), bookCostmap, balances, orderHistory))
-                        .catch(err => Logger.warn("Error getting order history from FTX", err));
-                });
-
-            }
+    function handleEvent(event, wsMarket, data, callback) {
+        if (data.e === event && data.wsMarket === wsMarket) {
+            callback(data);
+        } else {
+            Logger.warn(`unknown event received ${data.e} ${data.wsMarket}`);
         }
-    })
+    }
+
+    function handleSpotAggregateTrades(t) {
+
+        const price = Number.parseFloat(t.p);
+        const qty = Number.parseFloat(t.q);
+
+        ticker = CoinUtils.convertFromBinanceTicker(t.s);
+
+        var trade = {
+            "ticker": ticker,
+            "price": price,
+            "qty": qty,
+            "cons": Math.round(price * qty)
+        }
+
+        //Logger.info(`received new trade: ticker:${trade.ticker} price:${trade.price} qty:${trade.qty} consideration:${trade.cons}`);
+
+        const priceUpdate = handlePriceUpdate(trade);
+        const alerts = getAlertsForPrice(priceUpdate);
+
+        // update trade counts map
+        tradeCountsPerMinute.set(ticker, tradeCountsPerMinute.get(ticker) + 1);
+
+        sio.emit('price:update', [priceUpdate, alerts]);
+
+    }
+
+
+
 }
 
-function handlePriceUpdate(data) {
+function handlePriceUpdate(trade) {
 
-    const ticker = data.market;
-    const trades = data.data;
-    const largestTrade = extractLargestTrade(trades, ticker);
-    const last10VolumesQ = getVolumesQueue(ticker);
-    updateVolumesInQueue(trades, last10VolumesQ);
-    const sumLast10Volumes = last10VolumesQ.reduce((r, v) => r + v, 0);
+    const ticker = trade.ticker;
+    const volumesObj = getVolumesObj(ticker);
+
+    let newSumVol;
+    if(volumesObj.queue.length < MAX_VOLUMES_QUEUE_SIZE){
+        newSumVol = volumesObj.queue.reduce((r, v) => r + v, 0) + trade.cons;
+        updateVolumesObj(volumesObj, trade.cons, newSumVol);
+
+    } else {
+        newSumVol = volumesObj.sum - volumesObj.queue.peek() + trade.cons;
+        updateVolumesObj(volumesObj, trade.cons, newSumVol);
+    }
+
+    const sumVolumes = newSumVol;
+
 
     const bookCost = bookCostmap.get(ticker);
 
     priceUpdate = {
         ticker: ticker,
-        priceObj: largestTrade,
-        tradeVolume: sumLast10Volumes,
+        priceObj: trade,
+        tradeVolume: sumVolumes,
+        tradesPerSecond: 0,
         bookCost: bookCost == null ? 0 : bookCost
     }
 
     return priceUpdate;
 }
 
-function extractLargestTrade(trades) {
-    return trades.sort((a, b) =>
-            a.size - b.size
-    ).slice(-1)[0]
-}
 
-function getVolumesQueue(ticker) {
-
-    var volumeQ = tickersToLast10VolQueues.get(ticker);
-    if(volumeQ === undefined) {
-        const Deque = require("collections/deque");
+function getVolumesObj(ticker) {
+    var volumesObj = tickersToVolObjs.get(ticker);
+    if(volumesObj === undefined) {
         volumeQ = new Deque();
-        tickersToLast10VolQueues.set(ticker, volumeQ);
+        volumesObj = {queue: volumeQ, sum: null};
+        tickersToVolObjs.set(ticker, volumesObj);
     }
-    return volumeQ;
+    return volumesObj;
 }
 
-function updateVolumesInQueue(trades, volumeQueue) {
-    trades.forEach(t => {
-        volumeQueue.push(t.size * t.price);
-        if(volumeQueue.length > VOLUMES_QUEUE_SIZE) {
-            volumeQueue.shift();
-        }
-    });
+function updateVolumesObj(volumesObj, latestVolume, newVolSum) {
+    volumesObj.queue.push(latestVolume);
+    volumesObj.sum = newVolSum;
+    if(volumesObj.queue.length > MAX_VOLUMES_QUEUE_SIZE) {
+        volumesObj.queue.shift();
+    }
 }
-
 
